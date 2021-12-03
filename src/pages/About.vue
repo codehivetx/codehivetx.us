c113f5786271dd5dc30b0c5f0a47419da96bcd4e
Steven R. Loomis <srl295@gmail.com>
2021-12-03 11:53:49 -0600

    step zero point one

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>About Code Hive Tx, LLC</h1>
+    <p>Code Hive Tx, LLC is a small consultancy with a focus on internationalization and global enablement.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
+
+    <h1>Welcome to the Code Hive</h1>
+
+    <p>
+      Welcome to codehivetx.us
+    </p>
+
+    <p class="home-links">
+      GitHub: <a href="https://srl295.github.io/" target="_blank" rel="noopener">@srl295</a>
+    </p>
+
+    <hr/>
+
+    © 2021 Code Hive Tx, LLC
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
