9d4e4964f537a84536783610c9909f9cd0934d4d
Steven R. Loomis <srl295@gmail.com>
2023-04-21 18:46:58 -0500

    interim style cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+//TODO: sets body wid/hei = 100%
+// import 'ant-design-vue/dist/antd.css';
 import './style.css'
 import App from './App.vue'
 
