359844e4a2124158024e096aa24c43d7a5f386f1
Steven R. Loomis <srl295@gmail.com>
2023-04-21 18:56:23 -0500

    feat: add gtag

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
+import VueGtag from "vue-gtag";
+
 //TODO: sets body wid/hei = 100%
 // import 'ant-design-vue/dist/antd.css';
 import './style.css'
@@ -7,4 +9,13 @@ import App from './App.vue'
 
 createApp(App)
     .use(Antd)
+    .use(VueGtag, {
+        config: {
+            id: "G-QHK7PH5P59",
+            params: {
+                anonymize_ip: true,
+                send_page_view: true, // while we're not yet a SPA
+            }
+        }
+    })
     .mount('#app');
