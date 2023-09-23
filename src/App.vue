8f0aa13cd3e59c4ca9cd94d5cec5f6c36d0f8988
Steven R. Loomis <srl295@gmail.com>
2023-09-23 18:35:56 -0500

    feat(style): style mostly ok

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,38 @@
 <script setup lang="ts">
-// import About from './pages/About.vue';
-// import Bees from './pages/Bees.vue';
-// import Contact from './pages/Contact.vue';
-// import Index from './pages/Index.vue';
-// import PrivacyAgreement from './pages/PrivacyAgreement.vue';
-// import { defineComponent } from 'vue';
 </script>
 
 <template>
   <div>
     <center>
-      <router-link to="/"><img alt="[Code Hive Tx, LLC Logo]" src="./assets/codehivetx.png" height="50"/></router-link>
+      <router-link to="/"><img class="logo" alt="[Code Hive Tx, LLC Logo]" src="./assets/codehivetx.png" height="50" /></router-link>
+      <br />
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link> |
+      <router-link to="/bees">Bees</router-link> |
+      <router-link to="/privacy">Privacy</router-link>
     </center>
-  <!-- <a-carousel >
-    <Index/>
-    <hr/>
-    <a name="about" />
-    <About/>
-    <hr/>
-    <Contact/>
-    <hr/>
-    <a name="bees" />
-    <Bees/>
-    <hr/>
-    <PrivacyAgreement/> -->
-    <!-- router stuff here -->
     <router-view></router-view>
-    <hr/>
+    <hr />
     © 2021—{{ thisyear }} Code Hive Tx, LLC • <a href="https://github.com/codehivetx/codehivetx.us">Fork me on GitHub!</a>
-    • Social:         <code>@codehivetx</code> on <a href="https://twitter.com/codehivetx">Twitter</a> | <a href="https://github.com/codehivetx">GitHub</a>
-  <!-- </a-carousel> -->
+    • Social: <code>@codehivetx</code> on <a href="https://twitter.com/codehivetx">Twitter</a> | <a
+      href="https://github.com/codehivetx">GitHub</a>
   </div>
 </template>
 
 <style scoped>
-/* .logo {
+.logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 1em #302705aa);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
+a:hover {
+  filter: drop-shadow(0 0 1em #302705aa);
+}
 </style>
 
 <script lang="ts">
